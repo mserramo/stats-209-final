32660f797bf408559af3cb7850255b09d19e4159
mserramo <34524571+mserramo@users.noreply.github.com>
2023-12-08 01:09:35 -0800

    match more, match better

---
+++ martin.R
@@ -0,0 +1,5 @@
+## -----------------------------------------------------------------------------
+#Use knitr::purl to generate R script
+knitr::knit_hooks$set(purl = knitr::hook_purl)
+knitr::opts_chunk$set(echo = TRUE)
+
